4b07e2c0c8eec2e1093ce4ac635fa48842a29709
javimoya33 <javimoya33@gmail.com>
2023-05-19 16:15:45 +0200

    Clase Entidad Llave

---
+++ WebAPIAutores/Servicios/ServicioLlaves.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Servicios
+{
+    public class ServicioLlaves
+    {
+        private readonly ApplicationDbContext context;
+
+        public ServicioLlaves(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task CrearLlave(string usuarioId, TipoLlave tipoLlave)
+        {
+            var llave = Guid.NewGuid().ToString().Replace("-", "");
+
+            var llaveAPI = new LlaveAPI
+            {
+                Activa = true,
+                Llave = llave,
+                TipoLlave = tipoLlave,
+                UsuarioId = usuarioId
+            };
+
+            context.Add(llaveAPI);
+            await context.SaveChangesAsync();
+        }
+    }
+}
